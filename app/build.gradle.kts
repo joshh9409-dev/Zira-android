9a74b08d1060cbfd029e2ac52ab74be88da8a7e6
joshh9409-dev <joshh9409@gmail.com>
2026-09-17 23:23:07 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.zira.assistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     compileOptions {
@@ -38,9 +38,15 @@ kotlin {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("com.google.android.filament:filament-android:1.57.1")
+    implementation("com.google.android.filament:filament-utils-android:1.57.1")
+    implementation("com.google.android.filament:gltfio-android:1.57.1")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
